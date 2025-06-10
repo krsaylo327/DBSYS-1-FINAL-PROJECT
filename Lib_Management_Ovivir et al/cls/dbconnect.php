89c61573db64b964cba060b80b94b519b4838919
intRoberts <163328982+intRoberts@users.noreply.github.com>
2025-06-10 18:04:22 +0800

    Delete Lib_Management_Ovivir et al directory

--- Lib_Management_Ovivir et al/cls/dbconnect.php
+++
@@ -1,4 +0,0 @@
-<?php
-$con = mysqli_connect("localhost:3307","info3b","apdev","clsdb") or die(mysqli_error());
-//$con = mysqli_connect("localhost:3307","root","","clsdb") or die(mysqli_error());
-?>
